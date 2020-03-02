3f9edc2fc8bf0492d687cfbaf057a478aa749ada
Kevin Sheehan <kshhn12@gmail.com>
2020-03-02 12:54:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
-float first_number = 2;
-float second_number = 1;
+Float first_number = 2;
+Float second_number = 1;
 
-float sum = first_number + second_number;
-float difference = first_number - second_number;
-float product = first_number * second_number;
-float quotient = first_number / second_number;
+Float sum = first_number + second_number;
+Float difference = first_number - second_number;
+Float product = first_number * second_number;
+Float quotient = first_number / second_number;
